cf1e73e4d89f1fa5b24ec10139715451a76aa994
Aytan <64599177+aytanasgarli@users.noreply.github.com>
2020-09-02 00:34:27 +0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://colab.research.google.com/drive/1adREr_Skbu5RqG2CCKrtUYgN5KmU_Yvw?usp=sharing'
+export_file_name = 'trained_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['pos', 'neg']
 path = Path(__file__).parent
 
 app = Starlette()
